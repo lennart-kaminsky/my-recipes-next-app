1d307017faaf1422bb14906ef5576bb7287ba0de
Lennart Kaminsky <lennartkaminsky@googlemail.com>
2023-11-20 20:13:20 +0100

    list input

--- components/ListInput/index.js
+++ components/ListInput/index.js
@@ -1,17 +1,77 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { ButtonNoStyle } from "../Button";
+import useSWR from "swr";
 
 export default function ListInput() {
+  const {
+    data: shoppingList,
+    isLoading,
+    error,
+  } = useSWR("api/shoppinglists/current");
+
+  if (isLoading) return;
+  console.log("Achtung: ", shoppingList);
+
+  async function addProductToList(event) {
+    event.preventDefault();
+
+    const newProduct = { name: event.target.elements.inputListItem.value };
+
+    const productResponse = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    });
+    if (!productResponse.ok) {
+      console.log("Failed to add product:", productResponse.status);
+      throw new Error("Failed to add product");
+    }
+    const addedProductResponse = await productResponse.json();
+
+    const updatedShoppingList = {
+      ...shoppingList,
+      products: [
+        ...shoppingList.products,
+        { product: addedProductResponse._id },
+      ],
+    };
+    console.log("updatedShoppingList", updatedShoppingList);
+
+    const updatedShoppingListResponse = await fetch(
+      "/api/shoppinglists/current",
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedShoppingList),
+      }
+    );
+
+    if (updatedShoppingListResponse.ok) {
+      console.log("Shopping list successfully updated.");
+    } else {
+      console.log(
+        "Error updating shopping list:",
+        updatedShoppingListResponse.status
+      );
+    }
+  }
+
   return (
-    <StyledListForm>
-      <label htmlFor="inputListItem">
+    <StyledListForm onSubmit={addProductToList}>
+      <ButtonNoStyle type="submit">
         <FontAwesomeIcon icon={faPlus} />
-      </label>
+      </ButtonNoStyle>
       <StyledListInput
         id="inputListItem"
         name="inputListItem"
         type="text"
+        placeholder={isLoading ? "connecting to db..." : "new product"}
       ></StyledListInput>
     </StyledListForm>
   );
